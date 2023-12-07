6deafc683b9b825ebb5c588b9bfe05c84537124a
aimahusnain <aimahusnain@gmail.com>
2023-12-08 00:25:33 +0500

    Change Typing Animation

--- src/components/TypingAnimation.tsx
+++ src/components/TypingAnimation.tsx
@@ -7,7 +7,7 @@ const TypingAnimation = () => {
         options={{
           speed: 200,
           loop: true,
-          strings: ["Designer", "Developer"],
+          strings: ["Designing", "Development"],
           breakLines: false,
         }}
       />
