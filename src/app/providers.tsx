94e2109fe9b8a30c163ebec9138954d462e8b9e5
aimahusnain <aimahusnain@gmail.com>
2023-12-11 00:45:58 +0500

    Remove Providers.tsx and Try to make homepage more responsive

--- src/app/providers.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import React from "react";
-import { NextUIProvider } from "@nextui-org/react";
-
-export default function Providers({ children }: { children: React.ReactNode }) {
-  return <NextUIProvider>{children}</NextUIProvider>;
-}
