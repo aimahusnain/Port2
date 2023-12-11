601f2c94cdaaadaa91bd7e70e75be0dab9c2d77f
aimahusnain <aimahusnain@gmail.com>
2023-12-11 19:31:12 +0500

    decrease the size of image

--- src/components/HomeBanner.tsx
+++ src/components/HomeBanner.tsx
@@ -49,7 +49,7 @@ const HomeBanner = () => {
     >
       <div className="p-0">
         <div className="flex sm:flex-row flex-col !mx-0 h-full">
-          <div className="p-0 overflow-hidden sm:items-end items-start sm:justify-end justify-end flex flex-col sm:h-screen h-[500px] md:w-[50%] w-full bg-boo-700">
+          <div className="p-0 overflow-hidden sm:items-end items-start sm:justify-end justify-end flex flex-col sm:h-screen h-[450px] md:w-[50%] w-full bg-boo-700">
             <img
               className="m-0 p-0 mt-10 object-cover object-top w-full h-full"
               loading="lazy"
