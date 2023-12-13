24ba1d8118567bd0b91433dcc70471c5300cb861
aimahusnain <aimahusnain@gmail.com>
2023-12-13 19:15:12 +0500

    t

--- src/components/(blog)/About/InsightRoll.tsx
+++ src/components/(blog)/About/InsightRoll.tsx
@@ -4,7 +4,7 @@ const InsightRoll = ({ insights }) => {
   return (
     <div className="w-full w-screen sm:overflow-visible overflow-hidden">
 
-    <div className="bg-black w-full text-white dark:text-dark whitespace-nowrap overflow-hidden">
+    <div className="bg-black dark:bg-light text-white dark:text-dark w-full whitespace-nowrap overflow-hidden">
       <div className="animate-roll py-2 sm:py-3 flex items-center justify-center capitalize font-semibold tracking-wider text-sm sm:text-base">
         {insights.map((text, index) => (
           <div key={index} className="animate-item">
