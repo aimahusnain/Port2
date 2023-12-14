5728307641f36c772dc53425735a8d188620120a
aimahusnain <aimahusnain@gmail.com>
2023-12-14 13:35:00 +0500

    Add Blog again

--- src/components/(blog)/Blog/Categories.tsx
+++ src/components/(blog)/Blog/Categories.tsx
@@ -4,7 +4,7 @@ import Category from "./Category";
 
 const Categories = ({ categories, currentSlug }) => {
   return (
-    <div className=" px-0 md:px-10 sxl:px-20 mt-10 border-t-2 text-dark dark:text-light border-b-2 border-solid border-dark dark:border-light py-4 flex items-start flex-wrap font-medium mx-5 md:mx-10">
+    <div className=" px-0 md:px-10 xl:px-20 mt-10 border-t-2 text-dark dark:text-light border-b-2 border-solid border-dark dark:border-light py-4 flex items-start flex-wrap font-medium mx-5 md:mx-10">
       {categories.map((cat) => (
         <Category
           key={cat}
