575c2ff61ae7cccbd8337d369aeffe4ee43c2900
aimahusnain <aimahusnain@gmail.com>
2024-01-07 11:43:48 +0500

    gh

--- src/components/HomeBanner.tsx
+++ src/components/HomeBanner.tsx
@@ -57,7 +57,7 @@ const [imageLoaded, setImageLoaded] = useState(false);
       >
         <div className="p-0 mb-16">
         <div className="flex md:flex-row flex-col !mx-0 h-full">
-          <div className="md:pt-0 pt-2 p-0 overflow-hidden md:items-end items-start md:justify-end justify-end flex flex-col md:h-screen h-[450px] md:w-full w-full bg-boo-600">
+          <div className="md:pt-0 pt-2 p-0 overflow-hidden md:items-end items-start md:justify-end justify-end flex flex-col md:h-screen h-[450px] md:w-[50%] w-full bg-boo-600">
               {" "}
               <Carousel
                 className="sm:pt-0 pt-2 h-full overflow-hidden flex items-start justify-center"
